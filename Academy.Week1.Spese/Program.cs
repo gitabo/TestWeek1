7673207140f2bbd94232dc98ef2b14070e7a5c6d
gitabo <mauroabozzi@hotmail.it>
2021-09-10 09:14:21 +0200

    Aggiungere i file di progetto.

---
+++ Academy.Week1.Spese/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Academy.Week1.Spese
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
